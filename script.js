72031fe542fd67151ad4e349d77fa7908f435075
Arturs A <72090779+JeiKeiRei@users.noreply.github.com>
2024-06-05 12:52:16 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,15 +1,40 @@
 document.getElementById('dark-mode').addEventListener('click', function() {
-    document.body.classList.add('dark-mode');
+    setDarkMode();
 });
 
 document.getElementById('light-mode').addEventListener('click', function() {
-    document.body.classList.remove('dark-mode');
+    setLightMode();
 });
 
 document.getElementById('start').addEventListener('click', function() {
-    document.getElementById('section1').scrollIntoView({ behavior: 'smooth' });
+    scrollToSection('section1');
 });
 
 function scrollToSection(sectionId) {
     document.getElementById(sectionId).scrollIntoView({ behavior: 'smooth' });
+}
+
+function toggleMenu() {
+    var sidePanel = document.getElementById('sidePanel');
+    if (sidePanel.style.display === 'block') {
+        sidePanel.style.display = 'none';
+    } else {
+        sidePanel.style.display = 'block';
+    }
+}
+
+function setDarkMode() {
+    document.body.classList.add('dark-mode');
+    document.getElementById('dark-mode').disabled = true;
+    document.getElementById('light-mode').disabled = false;
+    document.getElementById('dark-mode-side').disabled = true;
+    document.getElementById('light-mode-side').disabled = false;
+}
+
+function setLightMode() {
+    document.body.classList.remove('dark-mode');
+    document.getElementById('dark-mode').disabled = false;
+    document.getElementById('light-mode').disabled = true;
+    document.getElementById('dark-mode-side').disabled = false;
+    document.getElementById('light-mode-side').disabled = true;
 }
